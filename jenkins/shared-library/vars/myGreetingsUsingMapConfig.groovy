297ab0057c7260bf3abb6e3e5b492c8229ca7a34
antonio.ochavo@solera.com <Antonio.Ochavo@solera.com>
2024-03-11 15:11:07 +0100

    no message

--- jenkins/shared-library/vars/myGreetingsUsingMapConfig.groovy
+++ jenkins/shared-library/vars/myGreetingsUsingMapConfig.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) {
 
     // WE SHOULD VALIDATE THE ENTRY!!! check if they are null, are strings, are valid dates, etc   
-    sh "echo Hola ${config.name}. Today is a great day: ${config.dayOfWeek}"
+    sh "echo Hola ${config.name}. ${config.dayOfWeek} is a great day: "
 }
